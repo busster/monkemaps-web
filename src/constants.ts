5bdff63803cbcd1d9f6bbf53b67aa7475f1c283d
thedonmon <22967202+thedonmon@users.noreply.github.com>
2022-06-14 19:34:47 -0500

    update to env

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const CONSTANTS = {
-  API_URL: 'http://localhost:4000/monkemaps',
-  MAPBOX_PLACES_API: 'https://api.mapbox.com/geocoding/v5/mapbox.places',
-  MAPBOX_ACCESS_TOKEN: 'pk.eyJ1IjoiamFzb25idXNzIiwiYSI6ImNsMnhxcWM3bzB5Y28zYnBmZGtrenhiZmMifQ.iNeJnRHRkvoKl5TnZvy8gg',
+  API_URL: process.env.REACT_APP_API_URL ?? 'http://localhost:4000/monkemaps',
+  MAPBOX_PLACES_API: process.env.REACT_APP_MAPBOX_PLACES_API ?? 'https://api.mapbox.com/geocoding/v5/mapbox.places',
+  MAPBOX_ACCESS_TOKEN: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
 }
