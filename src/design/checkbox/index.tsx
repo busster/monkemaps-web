e513054566cd38d48fe1124e139fdf2684b7fcfa
thedonmon <22967202+thedonmon@users.noreply.github.com>
2022-06-29 15:29:07 -0500

    add hardware wallet checkbox

---
+++ src/design/checkbox/index.tsx
@@ -0,0 +1,25 @@
+import './index.css';
+
+type MDCheckboxProps = {
+  checked: boolean,
+  setChecked: (checked: boolean) => void,
+}
+
+export const MDCheckbox = (props: MDCheckboxProps) => {
+  const {
+    checked,
+    setChecked,
+  } = props;
+
+  return (
+    <div>
+    <input
+      className={`md-switch ${checked ? 'md-switch-checked' : ''}`}
+      type="checkbox"
+      checked={checked}
+      onChange={() => setChecked(!checked)}
+    >
+    </input>
+    </div>
+  );
+}
