d5df615d1f2e2475ae58ec4699aaa31b77b417b2
thedonmon <22967202+thedonmon@users.noreply.github.com>
2023-01-16 02:13:37 -0500

    undo localstorage

--- src/utils/tokenUtils.ts
+++ src/utils/tokenUtils.ts
@@ -1,5 +1,5 @@
 export function getToken() {
-  const tokenString = sessionStorage.getItem('token') || '{}';
+  const tokenString = localStorage.getItem('token') || '{}';
   const userToken = JSON.parse(tokenString);
   return {
     token:
@@ -12,9 +12,9 @@ export function getToken() {
 }
 
 export function clearToken() {
-  const tokenString = sessionStorage.getItem('token') || '{}';
+  const tokenString = localStorage.getItem('token') || '{}';
   const userToken = JSON.parse(tokenString);
   if (userToken) {
-    sessionStorage.removeItem('token');
+    localStorage.removeItem('token');
   }
 }
